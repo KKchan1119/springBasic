7a44522f66dd7490920f67af7823c07d1fc631fb
KKchan1119 <kkchan1119@gmail.com>
2023-03-17 10:41:21 +0900

    article생성

---
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -0,0 +1,14 @@
+package com.ll.basic1.boundedContext.article.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+
+    @Id // primary key
+    private long id;
+    private String title;
+    private String body;
+}
